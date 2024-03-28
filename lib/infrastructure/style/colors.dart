ed5da067221893e3acf566d8c8cb9e14a6149714
Ahmet Baki Yucedag <yucedag2002@gmail.com>
2024-03-28 21:02:06 +0800

    Settings Screen Implemented

--- lib/infrastructure/style/colors.dart
+++ lib/infrastructure/style/colors.dart
@@ -4,3 +4,5 @@ Color lightGreenColor = const Color(0xFF00D6AA);
 Color darkBlueColor = const Color(0xFF000B35);
 Color lightGreyColor = const Color(0xFFA3A3A3);
 Color offWhiteColor = const Color(0xFFE7E7E7);
+Color semiBlueColor = const Color(0xFF18116F);
+Color darkRedColor = const Color(0xFF6F1111);
--- lib/presentation/settings/settings_screen.dart
+++ lib/presentation/settings/settings_screen.dart
@@ -1,10 +1,58 @@
+import 'package:budgetly/infrastructure/style/colors.dart';
+import 'package:budgetly/presentation/widgets/settingsbutton.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 20, 20, 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SettingsButton.customButton(
+            label: 'Edit Profile',
+            icon: Icons.arrow_forward,
+            backgroundColor: semiBlueColor,
+            onPressed: () {
+              Navigator.pushNamed(context, '/EditProfileScreen');
+            },
+            context: context,
+          ),
+          SizedBox(height: 16),
+          SettingsButton.customButton(
+            label: 'Change Password',
+            icon: Icons.arrow_forward,
+            backgroundColor: semiBlueColor,
+            onPressed: () {
+              Navigator.pushNamed(context, '/ChangePasswordScreen');
+            },
+            context: context,
+          ),
+          const SizedBox(height: 16),
+          SettingsButton.customButton(
+            label: 'Language',
+            icon: Icons.arrow_forward,
+            backgroundColor: semiBlueColor,
+            onPressed: () {
+              Navigator.pushNamed(context, '/LanguagesScreen');
+            },
+            context: context,
+          ),
+          SizedBox(height: 16),
+          SettingsButton.customButton(
+            label: 'Log Out',
+            backgroundColor: darkRedColor,
+            onPressed: () {
+              Navigator.pushNamed(context, '/Welcome');
+            },
+            context: context,
+          ),
+        ],
+      ),
+    );
   }
 }
